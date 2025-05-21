6da90544e36feb81c2767c064d68385384325f0d
Tim Kinnane <tim-kinnane@pluralsight.com>
2025-05-21 11:01:50 +1000

    chore: add cloneAll function for pulling org repos

--- environment/git.sh
+++ environment/git.sh
@@ -4,3 +4,19 @@
 export GIT_CEILING_DIRECTORIES=$HOME
 
 alias ghtoken="export GITHUB_TOKEN=\"$(gh auth token)\""
+
+alias gt="gittower ."
+
+# Utility to clone all repositories from a GitHub organization
+# Requires a GITHUB_TOKEN env var with repo access
+# Dependencies: gh, jq, parallel
+# Usage: CloneAll <organization>
+CloneAll() {
+  ORG=$1
+  page=1
+  while links=($(curl -H "Authorization: token ${GITHUB_TOKEN}" -s "https://api.github.com/orgs/${ORG}/repos?per_page=100&page=${page}" | jq -rc '.[] | {ssh_url} | .ssh_url'));  [[ "$links" ]]
+  do
+      GIT_TERMINAL_PROMPT=0 parallel git clone --depth=1 {} ::: "${links[@]}"
+      ((++page))
+  done
+}
